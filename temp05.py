e2ca108761cda454969afa50f6b6900a1a965c2c
Tzahi Cohen <6945700+mjhfvi@users.noreply.github.com>
2021-10-02 22:23:12 +0300

    add a note to file

--- temp05.py
+++ temp05.py
@@ -21,7 +21,7 @@ def FilePattern(Pattern):
             print("Found File: ", x)                            # print file after found bin in the name
             list01.append(x)                                    # appand the file to list01
             SortList.append(re.split('-', x)[3])                # appand only the 3 value after the "-" to the Sortlist
-            SortList.sort()
+            SortList.sort()                                     # sort the list
 
 for i in NamePatternList:
     print(FilePattern(i))
